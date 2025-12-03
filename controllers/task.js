c0cc122ef4bb07dd1f16ed0b25c80a86beb28948
Fostacz Léa <leafostacz@gmail.com>
2025-12-03 17:32:32 +0100

    début controllers

--- controllers/task.js
+++ controllers/task.js
@@ -1,9 +1,22 @@
 const TaskModel = require('../models/task.js');
 
 module.exports = {
-    cget: (req, res, next) => {},
-    post: (req, res, next) => {},
+    cget: async (req, res, next) => {
+        res.json(await TaskModel.findAll());
+    },
+    post: async (req, res, next) => {
+        const newData = req.body;
+        const newTask = await TaskModel.create(newData);
+        res.status(201).json(newTask);
+    },
     get: (req, res, next) => {},
     patch: (req, res, next) => {},
-    delete: (req, res, next) => {}
+    delete: async (req, res, next) => {
+        await TaskModel.destroy({
+            where: {
+                id: req.params.id
+            }
+        });
+        res.sendStatus(204);
+    }
 };
